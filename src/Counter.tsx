919e940e3d56592db1e9c14383667de801f25fb7
couvq <quentincouvelaire@gmail.com>
2023-10-16 16:01:45 -0500

    made increment and decrement operations use redux toolkit rather than native react state

--- src/Counter.tsx
+++ src/Counter.tsx
@@ -1,11 +1,15 @@
-import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { decrement, increment, incrementByAmount } from "./redux/counter";
 
 const Counter = () => {
-  const [count, setCount] = useState(0);
-  return <>
+  // take count state from counter reducer 
+const { count } = useSelector((state) => state.counter)
+const dispatch = useDispatch()
+return <>
     <h1>The count is: {count}</h1>
-    <button onClick={() => setCount(count + 1)}>increment</button>
-    <button onClick={() => setCount(count - 1)}>decrement</button>
+    <button onClick={() => dispatch(increment())}>increment</button>
+    <button onClick={() => dispatch(decrement())}>decrement</button>
+    <button onClick={() => dispatch(incrementByAmount(5))}>increment by 5</button>
   </>;
 };
 
